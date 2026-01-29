405a6f6b92b65143ec959e758628f70fd6675af4
aishuJL <143312597+aishuJL@users.noreply.github.com>
2026-01-29 13:25:00 +0530

    Time: 5 ms (98.95%), Space: 48.5 MB (97.81%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,9 +1,22 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int sum=0;
-        for(int i=0;i<nums.length;i+=2){
-            sum+=nums[i];
+        int[] freq = new int[20001]; // -10000 to 10000
+
+        for (int num : nums) {
+            freq[num + 10000]++;
+        }
+
+        int sum = 0;
+        boolean take = true;
+
+        for (int i = 0; i < freq.length; i++) {
+            while (freq[i] > 0) {
+                if (take) {
+                    sum += i - 10000;
+                }
+                take = !take;
+                freq[i]--;
+            }
         }
         return sum;
     }
